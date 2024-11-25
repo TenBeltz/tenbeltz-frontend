69d9df3964b14e42c2437dcada1384978d159540
Angel Jimenez <angeljimenezabreu2@gmail.com>
2024-11-25 15:14:01 -0500

    feat: add header, hero and banner sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,,html,js,jsx,md,mdx,svelte,ts,tsx,vue,svg}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'petal-plush': '#E5C6F3',
+				'blueberry-glaze': '#BE71E0',
+				'pheromone-purple': '#922ABF',
+				'sapphire-siren': '#6C208C',
+				'berry-blackmail': '#581A73',
+				'cold-heights': '#21EFE3',
+				'spiro-disco-ball': '#11B2FC',
+				'vivid-blue': '#2140FF',
+				'child-of-the-night': '#1A0076',
+				'white-smoke': 'F2F2F2',
+				'obsidian-shard': '1E293B'
+			}
+		},
 	},
 	plugins: [],
 }

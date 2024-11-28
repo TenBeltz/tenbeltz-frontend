4cb0eab94b8c399ade4e3014ec2d502926149a9a
Angel Jimenez <angeljimenezabreu2@gmail.com>
2024-11-27 20:24:07 -0500

    feat: add background images and configure path aliases

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [tailwind(), react()]
+  integrations: [tailwind()],
+  site: 'https://tenbeltz.com/',
 });

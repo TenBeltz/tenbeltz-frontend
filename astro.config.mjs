256923fd25feff740f8cb1f5891c843674e05999
Angel Jimenez <angeljimenezabreu2@gmail.com>
2024-11-29 16:13:06 -0500

    feat(services): replace static images with React SVG components
    - Service icons are now loaded as React SVG components.
    - Added support for customizing icons using the `fill`, `width`, and `height` props.
    - Removed the use of <Image> tag for service icons.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import svgr from '@svgr/rollup';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   site: 'https://tenbeltz.com/',
+  vite: {
+    plugins: [
+      svgr(),
+    ]
+  }
 });

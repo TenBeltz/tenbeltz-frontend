3e1e04d512b878c284045b0994486b5df4b7da0c
Aritz Jaber Lopes <aritzzjl@gmail.com>
2025-12-20 09:50:43 +0100

    feat: add @astrojs/node dependency and configure output settings in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import svgr from '@svgr/rollup';
+import node from '@astrojs/node';
 
 import react from '@astrojs/react';
 
@@ -10,6 +11,8 @@ export default defineConfig({
   devToolbar: {
     enabled: false
   },
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
   integrations: [react()],
   site: 'https://tenbeltz.com/',
   vite: {
